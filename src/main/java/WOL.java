316137b71e67d2ac78dc05d545c9c8e59b6d1bb4
Luigi Di Stefano <luigi.distefano.work@gmail.com>
2015-07-05 12:37:35 +0200

    Update WOL.java
    Refactored in order to have a useable class, added some usage instruction and some print features

--- src/main/java/WOL.java
+++ src/main/java/WOL.java
@@ -1,3 +1,5 @@
+package main.java;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -6,50 +8,133 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class WOL {
+	private static int WAKE_UP_PORT = 9;
 
-        public static void main(String[] args) throws IOException {
-                String macAddress = args[0];
-                int port = 9;
-                wakeUp(macAddress, port);
-        }
+	private String macAddress;
+	private int port;
 
-        private static void wakeUp(String macAddress, int port) throws IOException {
-                byte[] bytes = getMagicBytes(macAddress);
-                InetAddress address = getMulticastAddress();
-                send(bytes, address, port);
-        }
+	public WOL(String macAddress, Integer port) {
+		this.macAddress = macAddress;
+		if (port != null)
+			this.port = port;
+		else
+			this.port = WAKE_UP_PORT;
+	}
 
-        private static void send(byte[] bytes, InetAddress addr, int port)
-                        throws IOException {
-                DatagramPacket p = new DatagramPacket(bytes, bytes.length, addr, port);
-                new DatagramSocket().send(p);
-        }
+	public void wakeUp() throws IOException {
+		byte[] bytes = getMagicBytes(macAddress);
+		InetAddress address = getMulticastAddress();
+		send(bytes, address, port);
+	}
 
-        private static InetAddress getMulticastAddress()
-                        throws UnknownHostException {
-                return InetAddress.getByAddress(new byte[] {-1, -1, -1, -1});
-        }
+	private byte[] getMagicBytes(String macAddress) throws IOException {
+		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
 
-        private static byte[] getMagicBytes(String macAddress) throws IOException {
-                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+		for (int i = 0; i < 6; i++)
+			bytes.write(0xff);
 
-                for (int i = 0; i < 6; i++)
-                        bytes.write(0xff);
+		// MAC address are often presented on AA:BB:CC:DD:EE:FF, not in
+		// AABBCCDDEEFF form, so we strip ":"s out
+		if (macAddress.indexOf(':') != -1) {
+			macAddress = macAddress.replace(":", "");
+		}
+		// In Windows world, instead, MAC Address are in AA-BB-CC-DD-EE-FF form
+		if (macAddress.indexOf('-') != -1) {
+			macAddress = macAddress.replace("-", "");
+		}
 
-                byte[] macAddressBytes = parseHexString(macAddress);
-                for (int i = 0; i < 16; i++)
-                        bytes.write(macAddressBytes);
+		byte[] macAddressBytes = parseHexString(macAddress);
+		for (int i = 0; i < 16; i++)
+			bytes.write(macAddressBytes);
 
-                bytes.flush();
+		bytes.flush();
 
-                return bytes.toByteArray();
-        }
+		return bytes.toByteArray();
+	}
 
-        private static byte[] parseHexString(String string) {
-                byte[] bytes = new byte[string.length() / 2];
-                for (int i = 0, j = 0; i < string.length(); i += 2, j++)
-                        bytes[j] = (byte) Integer.parseInt(string.substring(i, i + 2), 16);
-                return bytes;
-        }
+	/**
+	 * Parse hex string
+	 * 
+	 * @param string
+	 * @return
+	 */
+	private byte[] parseHexString(String string) {
+		byte[] bytes = new byte[string.length() / 2];
+		for (int i = 0, j = 0; i < string.length(); i += 2, j++)
+			bytes[j] = (byte) Integer.parseInt(string.substring(i, i + 2), 16);
+		return bytes;
+	}
+
+	private void send(byte[] bytes, InetAddress addr, int port) throws IOException {
+		DatagramPacket p = new DatagramPacket(bytes, bytes.length, addr, port);
+		try (DatagramSocket s = new DatagramSocket()) {
+			System.out.println("Sending magic packet to " + this.macAddress);
+			s.send(p);
+
+		} catch (Error e) {
+			System.err.println(e);
+		}
+	}
+
+	private static InetAddress getMulticastAddress() throws UnknownHostException {
+		return InetAddress.getByAddress(new byte[] { -1, -1, -1, -1 });
+	}
+
+	/**
+	 * @return the macAddress
+	 */
+	public String getMacAddress() {
+		return macAddress;
+	}
+
+	/**
+	 * @param macAddress
+	 *            the macAddress to set
+	 */
+	public void setMacAddress(String macAddress) {
+		this.macAddress = macAddress;
+	}
+
+	/**
+	 * @return the port
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * @param port
+	 *            the port to set
+	 */
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	/**
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		if (args.length < 1) {
+			printUsage();
+			return;
+
+		} else {
+			System.out.println("Trying to wakeup " + args[0] + " on port " + WAKE_UP_PORT);
+			WOL wol = new WOL(args[0], WAKE_UP_PORT);
+			wol.wakeUp();
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private static void printUsage() {
+		StringBuffer usage = new StringBuffer();
+		usage.append("Usage: \n");
+		usage.append("\t WOL <mac_address_space_separated>");
+		System.out.println(usage);
+	}
+
 }
-
